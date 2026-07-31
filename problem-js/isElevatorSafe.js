4c2441e38f6d1f4748a7cf047944555f301c4257
tanjimtaosif <itsmahtab007@gmail.com>
2026-07-31 20:03:33 +0600

    feat: add isElevatorSafe function to validate elevator weight capacity

---
+++ problem-js/isElevatorSafe.js
@@ -0,0 +1,17 @@
+function isElevatorSafe(weights) {
+  if (!Array.isArray(weights)) {
+    return "Invalid";
+  }
+
+  let total = 0;
+  for (let i = 0; i < weights.length; i++) {
+    total += weights[i];
+  }
+
+  return total <= 400;
+}
+
+console.log(isElevatorSafe([60, 75, 50]));
+console.log(isElevatorSafe([90, 100, 95, 120]));
+console.log(isElevatorSafe([400]));
+console.log(isElevatorSafe("60,75,50"));
