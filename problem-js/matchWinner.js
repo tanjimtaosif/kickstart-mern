e5bc5498dd49fddf452a6a0e9f6e4342b34df9f8
tanjimtaosif <itsmahtab007@gmail.com>
2026-07-31 19:54:46 +0600

    feat: implement matchWinner function to determine game results based on goal counts

---
+++ problem-js/matchWinner.js
@@ -0,0 +1,18 @@
+function matchWinner(teamAGoals, teamBGoals) {
+  if (typeof teamAGoals !== "number" || typeof teamBGoals !== "number") {
+    return "Invalid";
+  }
+  if (teamAGoals > teamBGoals) {
+    return "Team A Won";
+  }
+  if (teamBGoals > teamAGoals) {
+    return "Team B Won";
+  }
+  return "Draw";
+}
+
+console.log(matchWinner(2, 1));
+console.log(matchWinner(1, 3));
+console.log(matchWinner(2, 2));
+console.log(matchWinner("3", 2));
+
